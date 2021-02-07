770a360f4d3078b6dbcee8788717c4f92eacb7e6
Geraldo Neto <geraldo.lopes@outlook.com>
2021-02-07 16:17:52 +0000

    validation

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -18,21 +18,21 @@ const landing = (props) => {
 const Landing = ({ articles, quickTips, refactorZone }) => {
   return (
     <div>
-      <Section path={`/articles`} text="Articles">
+      {articles && <Section path={`/articles`} text="Articles">
         <LatestPost
           title={articles[0].details.title}
           date={articles[0].details.date}
           link={`/articles/${articles[0].id}`}
         />
-      </Section>
-      <Section path={`/quick-tips`} text="Quick tips">
+      </Section>}
+      {quickTips && <Section path={`/quick-tips`} text="Quick tips">
         <LatestPost
           title={quickTips[0].details.title}
           date={quickTips[0].details.date}
           link={`/quick-tips/${quickTips[0].id}`}
         />
-      </Section>
-      <Section path={`/refactor-zone`} text="Refactor Zone" />
+      </Section>}
+      {refactorZone && <Section path={`/refactor-zone`} text="Refactor Zone" />}
     </div>
   );
 };
